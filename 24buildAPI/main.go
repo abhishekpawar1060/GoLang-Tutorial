70ba758350620ed4510b3aaae6e4abc8acd1b3ae
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-01-29 21:53:35 +0530

    buildAPI

---
+++ 24buildAPI/main.go
@@ -0,0 +1,26 @@
+package main
+
+//Model for cousre -file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename`
+	CoursePrice int     `json"Price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+//fake DB
+var courses []Course
+
+//middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+
+}
