4b044811b9a4055ac518cd509eff4ab23cec8e4a
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-01-30 22:15:28 +0530

    commit

--- 24buildAPI/main.go
+++ 24buildAPI/main.go
@@ -1,6 +1,14 @@
 package main
 
-//Model for cousre -file
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Model for cousre -file
 type Course struct {
 	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename`
@@ -13,14 +21,50 @@ type Author struct {
 	Website  string `json:"website"`
 }
 
-//fake DB
+// fake DB
 var courses []Course
 
-//middleware, helper - file
+// middleware, helper - file
 func (c *Course) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
 }
 
 func main() {
+
+}
+
+//Controllers - file
+//serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API</h1>"))
+
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all Course")
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//grab id from request
+	params := mux.Vars(r)
+
+	//loop through courses, find matching id and return the reponse
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Couse found with given id")
+	return
 
 }
