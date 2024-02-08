6f36d6d1ee84472080ac1aaf11588c2cb1eb4795
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-02-08 21:46:27 +0530

    commit

--- 25mongoAPI/controller/controller.go
+++ 25mongoAPI/controller/controller.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/abhishekpawar1060/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
@@ -35,4 +39,32 @@ func init() {
 
 	//collection instance
 	fmt.Println("Collection instance is Ready")
+}
+
+//MONGODB helpers -file
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inseted 1 moive in db with id: ", inserted.InsertedID)
+}
+
+// update 1 record
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
 }
