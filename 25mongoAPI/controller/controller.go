5508797d60b58eeb36d462fb1aaa80cd7c9fcb3b
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-02-09 22:26:49 +0530

    commit

--- 25mongoAPI/controller/controller.go
+++ 25mongoAPI/controller/controller.go
@@ -68,3 +68,53 @@ func updateOneMovie(movieId string) {
 
 	fmt.Println("modified count: ", result.ModifiedCount)
 }
+
+// delete 1 record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Movie got deleted with delete count: ", deleteCount)
+}
+
+// delete All recored from MongoDB
+func deleteAll() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of movie delete: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
+
+// get all movies from database
+func getAllMovie() []primitive.M {
+
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+
+	defer cursor.Close(context.Background())
+
+	return movies
+}
