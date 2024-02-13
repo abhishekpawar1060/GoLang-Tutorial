2a05e114808c4863c02ac248fe73c71bd2978bf4
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-02-13 21:47:36 +0530

    commit

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,18 +2,54 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
+
+var signals = []string{"test"}
+
+var wg sync.WaitGroup //pointer
+var mut sync.Mutex    //pointer
 
 func main() {
 	fmt.Println("Gorutines")
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+	websiteslist := []string{
+		"https://google.com",
+		"https://fb.com",
+		"https://go.dev",
+		"https://github.com",
+	}
+	for _, web := range websiteslist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
 }
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		panic(err)
+	} else {
+
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
+
 	}
 }
